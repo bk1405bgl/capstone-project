1ce89d3d218dbff921dafbe10abb8cd396ed4ad5
Bilal Kuzey <bk1405bgl@gmail.com>
2023-03-14 11:42:35 +0100

    add page styled a little bit a used for temporary console.logs

--- pages/add.js
+++ pages/add.js
@@ -4,16 +4,18 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Add({ data, error }) {
-  if (error) return <h2>Failed to load data..</h2>;
-  if (!data) return <h2>Loading...</h2>;
-
+  console.log(data);
+  if (error) return <h2>Comics konnten nicht geladen werden..</h2>;
+  if (!data) return <h2>Comics werden geladen...</h2>;
   return (
     <main>
+      <Link href="/collection">Zurück zur eigenen Sammlung</Link>
       <SubHeading>Verfügbare Comics</SubHeading>
+
       <div>
         {data.map((comic) => (
           <span key={comic.id}>
-            <Link href="/">
+            <Link href={`/${comic.id}`}>
               <Image
                 src={`${comic.thumbnail.path}/portrait_xlarge.${comic.thumbnail.extension}`}
                 alt={comic.title}
