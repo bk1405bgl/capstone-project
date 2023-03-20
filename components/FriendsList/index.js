36ff0df655e7a7367abfa7d917ff42c67a764989
Bilal Kuzey <bk1405bgl@gmail.com>
2023-03-20 10:15:59 +0100

    removed dead-links

--- components/FriendsList/index.js
+++ components/FriendsList/index.js
@@ -1,12 +1,8 @@
-import Link from "next/link";
-
 export default function FriendsList({ friendName }) {
   return (
     <section>
       <hr />
-      <span>
-        <Link href="#">{friendName}</Link>
-      </span>
+      <div>{friendName}</div>
     </section>
   );
 }
