3d9e80df9253dbff30ddafe9b1240b01a864530f
Bilal Kuzey <bk1405bgl@gmail.com>
2023-03-31 09:58:19 +0200

    deleted test file, which is not working

--- pages/comics/comics.test.js
+++
@@ -1,8 +0,0 @@
-import { screen } from "@testing-library/react";
-
-test("screens a heading", () => {
-  const heading = screen.getByRole("heading", {
-    name: /Comic-Sammlung/i,
-  });
-  expect(heading).toBeInTheDocument();
-});
